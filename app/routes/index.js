27d37aa953c95920d247f51a3c1e7f8bf4604f96
Suzi Rubino <suzi@suzirubi.com>
2017-01-08 21:27:40 -0800

    finish homework for super-rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,15 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
+  actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+  },
+    destroyRental(rental) {
+      rental.destroyRecord ();
+      this.transitionTo('index');
+    }
+  }
 });
